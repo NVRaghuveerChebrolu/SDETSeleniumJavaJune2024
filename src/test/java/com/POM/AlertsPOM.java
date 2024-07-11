8bdea0dfede3ad5d008a0582611ed61f270dfced
Naga Venkata Raghuveer Chebrolu <raghuchnv@gmail.com>
2024-07-11 08:24:53 +0530

    Alerts in selenium

---
+++ src/test/java/com/POM/AlertsPOM.java
@@ -0,0 +1,24 @@
+package com.POM;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AlertsPOM {
+	public WebDriver driver;
+
+	public AlertsPOM(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+
+	}
+
+	@FindBy(id = "alertButton")
+	public WebElement NormalAlertButton;
+	
+	@FindBy(id = "timerAlertButton")
+	public WebElement timerAlertButton;
+	
+
+}
